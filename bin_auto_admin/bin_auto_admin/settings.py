5b8b89c99fa6d0d96d6884b355b1968ad3185721
Veronique Doss <vdoss@student.21-school.ru>
2023-10-10 23:13:10 +0700

    deploy

--- bin_auto_admin/bin_auto_admin/settings.py
+++ bin_auto_admin/bin_auto_admin/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-s0du7zeg5^fxg3n$1k#kpox^9u0-9l+@%u^qtkj4y+3n=516$6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
