9e0878c4392f4add782197565011cb0d49be5425
Veronique Doss <vdoss@student.21-school.ru>
2023-10-10 20:07:21 +0700

    added field into table

--- bin_auto_admin/tables/models.py
+++ bin_auto_admin/tables/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class File(models.Model):
     file_name   = models.CharField(max_length=255)
     description = models.TextField()
+    car_model   = models.TextField()
     file_link   = models.URLField()
     price       = models.DecimalField(max_digits=10, decimal_places=2)
     created_at  = models.DateTimeField(auto_now_add=True)
